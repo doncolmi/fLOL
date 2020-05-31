375ae9a67d1b1e66966d48afdaffd1871f78e26f
doncolmi <matathresh1740@naver.com>
2020-06-01 02:03:48 +0900

    update 06-01

--- win/app.js
+++ win/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cor = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
+
+app.use(cor());
 
 app.use(logger('dev'));
 app.use(express.json());
